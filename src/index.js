b30d3f55b4c9a3bd8172d333cfd9b699bca5b8bb
Hain Thit <hainlthit@gmail.com>
2022-01-08 01:52:32 -0500

    commit tree bcf73f2999f700dc1f2101c42be8f12a19b587d2

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
 
+
+
 ReactDOM.render(<App />, document.getElementById("root"));
+
+const header = document.createElement('h1')
+header.innerText = "hello from the Header!"
